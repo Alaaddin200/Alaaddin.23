8ce33b4a41acce823542983fdaa54f8b7f6f2a92
Alaaddin200 <118636791+Alaaddin200@users.noreply.github.com>
2023-01-25 03:06:07 +0200

    0

--- Alaaddin.23/Program.cs
+++ Alaaddin.23/Program.cs
@@ -1,32 +1,95 @@
-﻿using System.Diagnostics;
-using System.Runtime.Remoting.Lifetime;
+﻿namespace EmployeeMgmt
+{
+    partial class Departments
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-Microsoft Visual Studio Solution File, Format Version 12.00
-# Visual Studio Version 17
-VisualStudioVersion = 17.4.33213.308
-MinimumVisualStudioVersion = 10.0.40219.1
-Project("{FAE04EC0-301F-11D3-BF4B-00C04F79EFBC}") = "EmployeeMgmt", "EmployeeMgmt\EmployeeMgmt.csproj", "{013DF1E4-8A4D-4327-8825-A6B04887DA2E}"
-EndProject
-Global
-	GlobalSection(SolutionConfigurationPlatforms) = preSolution
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-        Debug | Any CPU = Debug | Any CPU
-		Release|Any CPU = Release|Any CPU
-	EndGlobalSection
-	GlobalSection(ProjectConfigurationPlatforms) = postSolution
+        #region Windows Form Designer generated code
 
-        { 013DF1E4 - 8A4D - 4327 - 8825 - A6B04887DA2E}.Debug | Any CPU.ActiveCfg = Debug | Any CPU
-{ 013DF1E4 - 8A4D - 4327 - 8825 - A6B04887DA2E}.Debug | Any CPU.Build.0 = Debug | Any CPU
-{ 013DF1E4 - 8A4D - 4327 - 8825 - A6B04887DA2E}.Release | Any CPU.ActiveCfg = Release | Any CPU
-{ 013DF1E4 - 8A4D - 4327 - 8825 - A6B04887DA2E}.Release | Any CPU.Build.0 = Release | Any CPU
-EndGlobalSection
-	GlobalSection(SolutionProperties) = preSolution
-
-        HideSolutionNode = FALSE
-
-    EndGlobalSection
-    GlobalSection(ExtensibilityGlobals) = postSolution
-
-        SolutionGuid = { 634B60D9 - BCF1 - 49CA - 9E39 - A7AB6E5614F3}
-EndGlobalSection
-EndGlobal
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Departments));
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.DepNameTb = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.EditBtn = new System.Windows.Forms.Button();
+            this.EmpLbl = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(143, 181);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(116, 61);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 37;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Teal;
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 1014);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1388, 26);
+            this.panel2.TabIndex = 36;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.label3.ForeColor = System.Drawing.Color.Teal;
+            this.label3.Location = new System.Drawing.Point(54, 255);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(174, 28);
+            this.label3.TabIndex = 29;
+            this.label3.Text = "Department Name";
+            // 
+            // DepNameTb
+            // 
+            this.DepNameTb.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.DepNameTb.Location = new System.Drawing.Point(54, 300);
+            this.DepNameTb.Name = "DepNameTb";
+            this.DepNameTb.Size = new System.Drawing.Size(311, 34);
+            this.DepNameTb.TabIndex = 28;
