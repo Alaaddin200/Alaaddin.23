72c4a304a98682328986f8bbf00f462e8edcd5c0
Alaaddin200 <118636791+Alaaddin200@users.noreply.github.com>
2023-01-25 04:44:01 +0200

    Employee Management System Version

--- Alaaddin.23/Program.cs
+++ Alaaddin.23/Program.cs
@@ -153,3 +153,54 @@
             // 
             // label2
             // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(733, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(153, 28);
+            this.label2.TabIndex = 27;
+            this.label2.Text = "Manage Salary";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(630, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Management System Version 1.0";
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1388, 125);
+            this.panel1.TabIndex = 26;
+            // 
+            // EmpCb
+            // 
+            this.EmpCb.FormattingEnabled = true;
+            this.EmpCb.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.EmpCb.Location = new System.Drawing.Point(54, 308);
+            this.EmpCb.Name = "EmpCb";
+            this.EmpCb.Size = new System.Drawing.Size(311, 36);
+            this.EmpCb.TabIndex = 44;
+            this.EmpCb.SelectionChangeCommitted += new System.EventHandler(this.EmpCb_SelectionChangeCommitted);
+            // 
+            // DaysTb
+            // 
+            this.DaysTb.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.DaysTb.Location = new System.Drawing.Point(54, 427);
+            this.DaysTb.Name = "DaysTb";
+            this.DaysTb.Size = new System.Drawing.Size(311, 34);
+            this.DaysTb.TabIndex = 45;
+            // 
+            // PeriodTb
+            // 
