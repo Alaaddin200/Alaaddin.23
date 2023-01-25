0676d5298ca7482d7c9b877972b3c7cd5e105d10
Alaaddin200 <118636791+Alaaddin200@users.noreply.github.com>
2023-01-25 04:41:03 +0200

    ComponentResourceManager

--- Alaaddin.23/Program.cs
+++ Alaaddin.23/Program.cs
@@ -1,118 +1,55 @@
-﻿<? xml version = "1.0" encoding = "utf-8" ?>
-< root >
-  < !--
-Microsoft ResX Schema
+﻿namespace EmployeeMgmt
+{
+    partial class Salaries
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-    Version 2.0
-    
-    The primary goals of this format is to allow a simple XML format 
-    that is mostly human readable. The generation and parsing of the 
-    various data types are done through the TypeConverter classes 
-    associated with the data types.
-    
-    Example:
-    
-    ... ado.net / XML headers & schema...
-    < resheader name = "resmimetype" > text / microsoft - resx </ resheader >
-    < resheader name = "version" > 2.0 </ resheader >
-    < resheader name = "reader" > System.Resources.ResXResourceReader, System.Windows.Forms, ...</ resheader >
-    < resheader name = "writer" > System.Resources.ResXResourceWriter, System.Windows.Forms, ...</ resheader >
-    < data name = "Name1" >< value > this is my long string</ value >< comment > this is a comment </ comment ></ data >
-    < data name = "Color1" type = "System.Drawing.Color, System.Drawing" > Blue </ data >
-    < data name = "Bitmap1" mimetype = "application/x-microsoft.net.object.binary.base64" >
-        < value >[base64 mime encoded serialized.NET Framework object] </ value >
-    </ data >
-    < data name = "Icon1" type = "System.Drawing.Icon, System.Drawing" mimetype = "application/x-microsoft.net.object.bytearray.base64" >
-        < value >[base64 mime encoded string representing a byte array form of the.NET Framework object] </ value >
-        < comment > This is a comment </ comment >
-    </ data >
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-    There are any number of "resheader" rows that contain simple 
-    name/value pairs.
-    
-    Each data row contains a name, and value. The row also contains a 
-    type or mimetype. Type corresponds to a .NET class that support
-    text/value conversion through the TypeConverter architecture. 
-    Classes that don't support this are serialized and stored with the 
-    mimetype set.
-    
-    The mimetype is used for serialized objects, and tells the 
-    ResXResourceReader how to depersist the object. This is currently not 
-    extensible. For a given mimetype the value must be set accordingly:
-    
-    Note - application / x - microsoft.net.object.binary.base64 is the format
-    that the ResXResourceWriter will generate, however the reader can 
-    read any of the formats listed below.
-    
-    mimetype: application / x - microsoft.net.object.binary.base64
-    value: The object must be serialized with 
-            : System.Runtime.Serialization.Formatters.Binary.BinaryFormatter
-            : and then encoded with base64 encoding.
-     mimetype: application / x - microsoft.net.object.soap.base64
-    value: The object must be serialized with 
-            : System.Runtime.Serialization.Formatters.Soap.SoapFormatter
-            : and then encoded with base64 encoding.
-    mimetype: application / x - microsoft.net.object.bytearray.base64
-    value: The object must be serialized into a byte array 
-            : using a System.ComponentModel.TypeConverter
-            : and then encoded with base64 encoding.
-    -->
-  <xsd:schema id = "root" xmlns="" xmlns:xsd = "http://www.w3.org/2001/XMLSchema" xmlns: msdata = "urn:schemas-microsoft-com:xml-msdata" >
-    < xsd:import namespace="http://www.w3.org/XML/1998/namespace" />
-    <xsd:element name = "root" msdata:IsDataSet="true">
-      <xsd:complexType>
-        <xsd:choice maxOccurs = "unbounded" >
-          < xsd:element name = "metadata" >
-            < xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name = "value" type="xsd:string" minOccurs="0" />
-              </xsd:sequence>
-              <xsd:attribute name = "name" use="required" type="xsd:string" />
-              <xsd:attribute name = "type" type="xsd:string" />
-              <xsd:attribute name = "mimetype" type="xsd:string" />
-              <xsd:attribute ref="xml:space" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name = "assembly" >
-            < xsd:complexType>
-              <xsd:attribute name = "alias" type="xsd:string" />
-              <xsd:attribute name = "name" type="xsd:string" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name = "data" >
-            < xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name = "value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
-                <xsd:element name = "comment" type="xsd:string" minOccurs="0" msdata:Ordinal="2" />
-              </xsd:sequence>
-              <xsd:attribute name = "name" type="xsd:string" use="required" msdata:Ordinal="1" />
-              <xsd:attribute name = "type" type="xsd:string" msdata:Ordinal="3" />
-              <xsd:attribute name = "mimetype" type="xsd:string" msdata:Ordinal="4" />
-              <xsd:attribute ref="xml:space" />
-            </xsd:complexType>
-          </xsd:element>
-          <xsd:element name = "resheader" >
-            < xsd:complexType>
-              <xsd:sequence>
-                <xsd:element name = "value" type="xsd:string" minOccurs="0" msdata:Ordinal="1" />
-              </xsd:sequence>
-              <xsd:attribute name = "name" type="xsd:string" use="required" />
-            </xsd:complexType>
-          </xsd:element>
-        </xsd:choice>
-      </xsd:complexType>
-    </xsd:element>
-  </xsd:schema>
-  <resheader name = "resmimetype" >
-    < value > text / microsoft - resx </ value >
-  </ resheader >
-  < resheader name="version">
-    <value>2.0</value>
-  </resheader>
-  <resheader name = "reader" >
-    < value > System.Resources.ResXResourceReader, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
-  </resheader>
-  <resheader name = "writer" >
-    < value > System.Resources.ResXResourceWriter, System.Windows.Forms, Version=4.0.0.0, Culture=neutral, PublicKeyToken=b77a5c561934e089</value>
-  </resheader>
-</root>
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Salaries));
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.EmpCb = new System.Windows.Forms.ComboBox();
+            this.DaysTb = new System.Windows.Forms.TextBox();
+            this.PeriodTb = new System.Windows.Forms.DateTimePicker();
+            this.LogoutLbl = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.SalaryLbl = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.AmountTb = new System.Windows.Forms.TextBox();
