c33ae61f9b7bb2423cb9b963c7cbe6d4dcf59c9b
Alaaddin200 <118636791+Alaaddin200@users.noreply.github.com>
2023-01-25 03:19:32 +0200

    Required designer variable.

--- Alaaddin.23/Program.cs
+++ Alaaddin.23/Program.cs
@@ -1,26 +1,101 @@
-﻿< Project Sdk = "Microsoft.NET.Sdk" >
+﻿namespace EmployeeMgmt
+{
+    partial class Employees
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
 
-  < PropertyGroup >
-    < OutputType > WinExe </ OutputType >
-    < TargetFramework > net6.0 - windows </ TargetFramework >
-    < Nullable > enable </ Nullable >
-    < UseWindowsForms > true </ UseWindowsForms >
-    < ImplicitUsings > enable </ ImplicitUsings >
-  </ PropertyGroup >
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
 
-  < ItemGroup >
-    < Compile Update = "Properties\Resources.Designer.cs" >
-      < DesignTime > True </ DesignTime >
-      < AutoGen > True </ AutoGen >
-      < DependentUpon > Resources.resx </ DependentUpon >
-    </ Compile >
-  </ ItemGroup >
+        #region Windows Form Designer generated code
 
-  < ItemGroup >
-    < EmbeddedResource Update = "Properties\Resources.resx" >
-      < Generator > ResXFileCodeGenerator </ Generator >
-      < LastGenOutput > Resources.Designer.cs </ LastGenOutput >
-    </ EmbeddedResource >
-  </ ItemGroup >
-
-</ Project >
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Employees));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.EmpNameTb = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.DailySalTb = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.GenCb = new System.Windows.Forms.ComboBox();
+            this.EmpCb = new System.Windows.Forms.ComboBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.DOBTb = new System.Windows.Forms.DateTimePicker();
+            this.JDateTb = new System.Windows.Forms.DateTimePicker();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.label9 = new System.Windows.Forms.Label();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.label11 = new System.Windows.Forms.Label();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label12 = new System.Windows.Forms.Label();
+            this.Delete = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Teal;
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1388, 125);
+            this.panel1.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(630, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(386, 28);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Employee Management System Version 1.0";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label2.ForeColor = System.Drawing.Color.Teal;
+            this.label2.Location = new System.Drawing.Point(734, 214);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(195, 28);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Manage Employees";
+            // 
+            // label3
+            // 
