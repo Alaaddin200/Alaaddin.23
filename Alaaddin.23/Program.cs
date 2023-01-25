8dfe98d4ffcdecbb22a7543f0c754e69e3538093
Alaaddin200 <118636791+Alaaddin200@users.noreply.github.com>
2023-01-25 04:49:33 +0200

    EmpCb.DataSource = Con.GetData(Query)

--- Alaaddin.23/Program.cs
+++ Alaaddin.23/Program.cs
@@ -56,4 +56,72 @@ namespace EmployeeMgmt
                 d = Convert.ToInt32(DaysTb.Text);
                 AmountTb.Text = "Rs " + (d * DSal);
             }
-            
+            //MessageBox.Show("" + DSal);
+            //EmpCb.DataSource = Con.GetData(Query);
+
+        }
+
+        private void ShowSalaries()
+        {
+            string Query = "Select * from SalaryTb1";
+            SalaryList.DataSource = Con.GetData(Query);
+        }
+
+        private void Salaries_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SalaryLbl_Click(object sender, EventArgs e)
+        {
+            Salaries Obj = new Salaries();
+            Obj.Show();
+            this.Hide();
+        }
+        int d = 1;
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                if (EmpCb.SelectedIndex == -1 || DaysTb.Text == "" || PeriodTb.Text == "")
+                {
+                    MessageBox.Show("Missing Data!!");
+                }
+                else
+                {
+                    period = PeriodTb.Value.Date.Month.ToString() + "-" + PeriodTb.Value.Date.Year.ToString();
+                    int Amount = DSal * Convert.ToInt32(DaysTb.Text);
+                    int Days = Convert.ToInt32(DaysTb.Text);
+
+
+                    string Query = "insert into SalaryTb1 values({0},{1},'{2}',{3},'{4}')";
+                    Query = string.Format(Query, EmpCb.SelectedValue.ToString, Days, period, Amount, DateTime.Today);
+                    Con.SetData(Query);
+                    ShowSalaries();
+                    MessageBox.Show("Salary Paid!!");
+                    DaysTb.Text = "";
+
+                }
+
+            }
+            catch (Exception Ex)
+            {
+                MessageBox.Show(Ex.Message);
+            }
+
+
+        }
+
+        private void EmpCb_SelectionChangeCommitted(object sender, EventArgs e)
+        {
+            GetSalary();
+        }
+
+        private void LogoutLbl_Click(object sender, EventArgs e)
+        {
+            Login Obj = new Login();
+            Obj.Show();
+            this.Hide();
+        }
+    }
+}
